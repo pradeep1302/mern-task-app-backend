ce35e4168e7261425dfe4bbc4c499c54b9b352b6
Pradeep Agrawal <pkagrawal181@gmail.com>
2023-08-24 13:02:13 +0530

    inital commit

---
+++ backend/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const Task = require("../model/taskModel")
+const router = express.Router()
+const {createTask, getTasks, getTask, deleteTask, updateTask}=require("../controllers/taskController")
+
+router.post("/api/tasks", createTask)
+router.get("/api/tasks", getTasks)
+router.get("/api/tasks/:id", getTask)
+router.delete("/api/tasks/:id", deleteTask)
+router.put("/api/tasks/:id", updateTask)
+// router.patch("/api/tasks/:id", updateTask)
+
+module.exports = router
---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const dotenv = require("dotenv").config()
+const express = require("express")
+const connectDB=require("./config/connectDB")
+const app = express()
+const mongoose = require("mongoose")
+const Task = require("./model/taskModel")
+const taskRoutes = require("./routes/taskRoutes")
+const cors = require("cors")
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(cors({
+    origin: ["http://localhost:3000", "https://mern-task-app.onrender.com"]
+}))
+app.use(taskRoutes)
+
+app.get("/",(req,res)=>{
+    res.send("Home Page");
+})
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+mongoose 
+    .connect(process.env.MONGO_URI)
+    .then(()=>{
+        app.listen(PORT,()=>{
+            console.log(`Server running on port ${PORT}`); 
+        })
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
